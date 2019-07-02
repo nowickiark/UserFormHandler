13fea9333caf8c39a7228acd8072257c82f37053
nowickiark@outlook.com <amrentos103>
2019-07-02 22:51:57 +0200

    Created overall model for handling new users with their messages.

---
+++ src/main/java/com/userForm/UserFormHandler/service/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.userForm.UserFormHandler.service;
+
+import com.userForm.UserFormHandler.data.UserDataSet;
+import com.userForm.UserFormHandler.model.UserModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+
+    private UserDataSet userDataSet;
+
+    @Autowired
+    public UserServiceImpl(UserDataSet userDataSet) {
+        this.userDataSet = userDataSet;
+    }
+
+
+
+    @Override
+    public List<UserModel> getUsers() {
+        return userDataSet.getUsers();
+    }
+
+    @Override
+    public UserModel getUserByName(String name) {
+
+        List<UserModel> tempUserList = userDataSet.getUsers();
+
+        Optional<UserModel> matchingUser = tempUserList.stream().filter(a -> a.getUserName().equals(name)).findFirst();
+
+        UserModel matchingUserModel = matchingUser.orElse(null);
+
+        return matchingUserModel;
+    }
+}
