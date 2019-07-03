de282574d6df62933913478eb36ec36e85016065
nowickiark@outlook.com <amrentos103>
2019-07-03 23:07:31 +0200

    New Usermorm html/css tamplate. Added http post method from template. Added recining controler of post method variable.
    To fix after post method updated userform with confirmation.

---
+++ src/main/resources/static/main.js
@@ -0,0 +1,27 @@
+const contactInputs = document.querySelectorAll('.contact-input'); //znajdujemy wszystkie lementy z klasy contact-input
+
+console.log('Dziala')
+
+for(let i = 0; i < contactInputs.length; i++){
+    //wewnątrz pętli musimy dostać dostę do lebela, czyli input name itd.
+    let currentLabel = contactInputs[i].parentElement.firstElementChild; //uzyskujemy dostęp do pierwszego elementu child
+    
+    //potrzebujemy dodać nasłuch na zdarzenie focus, czyli najechanie kursorem na pola input
+
+    contactInputs[i].addEventListener('focus', function(){
+        currentLabel.classList.add('move-up');
+    });
+
+    contactInputs[i].addEventListener('blur', function(){
+        if(contactInputs[i].value === '')
+        currentLabel.classList.remove('move-up')
+    });
+
+}
+
+function submit_byName(){
+
+var forms = document.getElementsByName('contactForm');
+forms[0].submit();
+
+}
