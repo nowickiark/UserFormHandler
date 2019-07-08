6ed51047a0faf1176b642e11b984370d778744f0
nowickiark@outlook.com <amrentos103>
2019-07-08 22:53:48 +0200

    Constant problem with post method from webite.

---
+++ src/main/java/com/userForm/UserFormHandler/controller/UserRestController.java
@@ -0,0 +1,45 @@
+package com.userForm.UserFormHandler.controller;
+
+import com.userForm.UserFormHandler.model.UserModel;
+import com.userForm.UserFormHandler.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserRestController {
+
+private UserService userService;
+
+@Autowired
+public UserRestController (UserService userService){this.userService = userService;}
+
+
+    @PostMapping("/userForm")
+    @ResponseBody
+    public UserModel addUserToList(@RequestParam ("name") String name, @RequestParam ("email") String email, @RequestParam("message") String message, Model model ) {
+        System.out.println(email);
+        System.out.println(name);
+        System.out.println(message);
+
+        UserModel formUser;
+
+        if (userService.doesUserExist(name)) {
+            formUser = userService.getUserByName(name);
+            model.addAttribute("name", "User " + name + " is already registared" + " message was added to his history");
+        } else {
+
+            formUser = new UserModel(name, email);
+            userService.addUser(formUser);
+            model.addAttribute("name", "Registered user " + name + " with message.");
+        }
+
+        formUser.addMessage(message);
+
+        return formUser;
+
+    }
+}
